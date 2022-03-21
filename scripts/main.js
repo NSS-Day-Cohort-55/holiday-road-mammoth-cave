1721525676c903d56a030cdcb51d3cbed23cb451
Houston-Smith <houstonbsmith@gmail.com>
2022-03-21 11:58:49 -0500

    Sliding Scroll to Top of Page

--- scripts/main.js
+++ scripts/main.js
@@ -11,39 +11,59 @@ applicationElement.addEventListener("click", event => {
 	if (event.target.id === "Plan_Trip"){
     ClearHome()
     ShowPlanner()
-    window.scrollTo(0, 250);
-	}
+    window.scrollTo({
+        top: 250,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
 })
 
 applicationElement.addEventListener("click", event => {
 	if (event.target.id === "Planner_Iteneraries"){
     ClearPlanner()
     ShowItenerary()
-    window.scrollTo(0, 250);
-	}
+    window.scrollTo({
+        top: 250,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
 })
 
 applicationElement.addEventListener("click", event => {
 	if (event.target.id === "Iteneraries_New"){
     ClearItenerary()
     ShowPlanner()
-    window.scrollTo(0, 250);
-	}
+    window.scrollTo({
+        top: 250,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
 })
 
 applicationElement.addEventListener("click", event => {
 	if (event.target.id === "Iteneraries_Reset"){
     ClearItenerary()
     ShowHome()
-    window.scrollTo(0, 250);
-	}
+    window.scrollTo({
+        top: 250,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
 })
 
 applicationElement.addEventListener("click", event => {
 	if (event.target.id === "Planner_State"){
     ClearPlanner()
     ShowHome()
-    window.scrollTo(0, 250);
-	}
+    window.scrollTo({
+        top: 250,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
 })
 
