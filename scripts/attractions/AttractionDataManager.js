0246f10fb8ca905c5e5156d3b7a953e3228169c3
ToryT23 <torjtheu@gmail.com>
2022-03-22 16:31:10 -0500

    curly braces

--- scripts/attractions/AttractionDataManager.js
+++ scripts/attractions/AttractionDataManager.js
@@ -1,35 +1,35 @@
-let attractionApi = "http://holidayroad.nss.team/bizarreries"
+let attractionApi = "http://holidayroad.nss.team/bizarreries";
 
 export const fetchAttraction = () => {
-    return fetch("http://holidayroad.nss.team/bizarreries")
-    .then(response => response.json())
-    // staged data to be used on line 16
-    // .then(parsedResponse => {
-    //     // do something with response here
-    //     return console.log(parsedResponse)
-    // })
-}
+  return fetch("http://holidayroad.nss.team/bizarreries").then((response) =>
+    response.json()
+  );
+  // staged data to be used on line 16
+  // .then(parsedResponse => {
+  //     // do something with response here
+  //     return console.log(parsedResponse)
+  // })
+};
 
 export const AttractionHtml = () => {
-    const renderAttraction = document.querySelector(".Planner_Selectors_Bazar")
-    fetchAttraction()
-    .then( (attractionApi) => {
-        
-     renderAttraction.innerHTML  = ` <h1>Bizarre</h1> <select id="selectedAttraction" >
+  const renderAttraction = document.querySelector(".Planner_Selectors_Bazar");
+  fetchAttraction().then((attractionApi) => {
+    renderAttraction.innerHTML = ` <h1>Bizarre</h1> <select id="selectedAttraction" >
         <option selected disabled hidden >Select an Attraction</option>
-        ${attractionApi.map((attraction) => `<option value=${attraction.id}>${attraction.name}</option>`)   }
-        </select>
-        `
-    })}
+        ${attractionApi.map(
+          (attraction) =>
+            `<option value=${attraction.id}>${attraction.name}</option>`
+        )}
+        </select>    `;
+  });
+};
 
-
-export const AttractionDetail = (attractionValue ) => {
-    const renderDetails = document.querySelector(".Planner_Details_Bazar")
-   fetchAttraction()
-   .then ( (bArray) => {
-        bArray.find( (bObj) =>  {
-           if (bObj.id === parseInt(attractionValue)) {
-               renderDetails.innerHTML =` 
+export const AttractionDetail = (attractionValue) => {
+  const renderDetails = document.querySelector(".Planner_Details_Bazar");
+  fetchAttraction().then((bArray) => {
+    bArray.find((bObj) => {
+      if (bObj.id === parseInt(attractionValue)) {
+        renderDetails.innerHTML = ` 
                <div>
                Description: ${bObj.description}  
                </div>
@@ -39,19 +39,19 @@ export const AttractionDetail = (attractionValue ) => {
                <div>
                Restrooms: ${bObj.ameneties.restrooms}
                </div>
-                `
-           }
-        }) 
-   })
-}
+                `;
+      }
+    });
+  });
+};
 
-const mainContainer = document.querySelector(".Holiday-Road")
+const mainContainer = document.querySelector(".Holiday-Road");
 
-mainContainer.addEventListener("change", changeEvent => {
-    if (changeEvent.target.id === "selectedAttraction") {
-        // Get what the user typed into the form fields
-        const attractionD = document.querySelector("#selectedAttraction").value
-         console.log(attractionD)
-       AttractionDetail(attractionD)
-}})
-
+mainContainer.addEventListener("change", (changeEvent) => {
+  if (changeEvent.target.id === "selectedAttraction") {
+    // Get what the user typed into the form fields
+    const attractionD = document.querySelector("#selectedAttraction").value;
+    console.log(attractionD);
+    AttractionDetail(attractionD);
+  }
+});
